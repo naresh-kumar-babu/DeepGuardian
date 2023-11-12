00baeee853431ff98805b40fa3067199c85a1154
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2023-11-12 14:42:21 +0530

    Remove URL input

--- predictor/urls.py
+++ predictor/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', home, name='home'),
-    path('verify/', verify, name='verify'),
-    path('results/<face>/', results, name='results'),
+    path('inspect/', verify, name='inspect'),
     path('about/', about, name='about')
 ]
--- predictor/views.py
+++ predictor/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 import requests
 from PIL import Image
 from .pipeline import loaded_pipeline as pipeline
@@ -11,27 +11,18 @@ def home(request):
 
 def verify(request):
     if request.method == 'POST':
-        url_input = request.POST.get('url_input')
         file_input = request.FILES.get('file_input')
-        if (url_input == '') and (file_input == ''):
-            return render(request, 'predictor/inspect.html', {'error': 'Please choose the image in either of the two formats'})
+        if file_input == '':
+            return render(request, 'predictor/inspect.html', {'error': 'Please choose the image before proceeding'})
         else:
-            if url_input:
-                im = Image.open(requests.get(url_input, stream=True).raw)
-                if im.mode == 'CMYK':
-                    im = im.convert('RGB')
-                im.save('DeepGuardian/static/img/input_img.jpeg')
-                result = pipeline.predict(image_path=im)
-                return image_chooser(request, result)
-            elif file_input:
-                image = ImagePicker(image = file_input)
-                image.save()
-                im = Image.open(image.image)
-                im.save('DeepGuardian/static/img/input_img.jpeg')
-                if im.mode == 'CMYK':
-                    im = im.convert('RGB')
-                result = pipeline.predict(image_path=im)
-                return image_chooser(request, result)
+            image = ImagePicker(image = file_input)
+            image.save()
+            im = Image.open(image.image)
+            im.save('DeepGuardian/static/img/input_img.jpeg')
+            if im.mode == 'CMYK':
+                im = im.convert('RGB')
+            result = pipeline.predict(image_path=im)
+            return image_chooser(request, result)
     return render(request, 'predictor/inspect.html')
 
 def image_chooser(request, result):
@@ -63,19 +54,6 @@ def image_chooser(request, result):
         face_score = face_score[:face_score.index('.')+3]
         st.append({'image':img, 'count':count, 'score': str(face_score) + ' %', 'color': color})
     return render(request, 'predictor/select.html', {'faces': st, 'overall_score': overall_score, 'overall_result': status})
-
-def results(request, face):
-    im = Image.open('DeepGuardian/static/img/face_{0}.jpeg'.format(face))
-    filepath = 'img/face_{0}.jpeg'.format(face)
-    result = pipeline.predict(image_path=im)
-    result = result['scores'][0]
-    fake_percentile = int(ceil(result*100))
-    status = ''
-    if fake_percentile > 50:
-        status = 'FAKE'
-    else:
-        status = 'REAL'
-    return render(request, 'predictor/results.html', {'fake':fake_percentile, 'filepath':filepath, 'status': status})
 
 def about(request):
     return render(request, 'predictor/about.html')
